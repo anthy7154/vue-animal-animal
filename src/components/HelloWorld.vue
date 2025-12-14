3c34ac95aa0266f00483328eaa5d74fdb9a1756f
anthy7154 <anthy7154@gmail.com>
2025-12-15 01:13:06 +0800

    feat: animation of showing image with music

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from 'vue'
-import { onBeforeUnmount, computed, onMounted, onUnmounted, watch } from 'vue'
+import { ref, onBeforeUnmount, onUnmounted, watch, onMounted } from 'vue'
+import animalMusic from '../assets/animal-music.mp3'
 
 defineProps({
   msg: String,
@@ -32,8 +32,8 @@ function changeFile(e) {
 
   // 產生隨機數字陣列並開始跑動畫
   randomIndex()
-  run()
 }
+
 
 // 處理上傳圖片後畫面重整
 onBeforeUnmount(() => {
@@ -64,12 +64,14 @@ const randomIndex = () => {
 
 // ============
 // highlight
-const currentIndex = ref(0)
+const currentIndex = ref(-1)
 const intervalId = ref(0)
 const run = () => {
-  intervalId.value = setInterval(() => {
-    currentIndex.value < 8 ? currentIndex.value++ : currentIndex.value = 0;
-  }, 500);
+  if (gameStatus.value) {
+    intervalId.value = setInterval(() => {
+      currentIndex.value < 8 ? currentIndex.value++ : currentIndex.value = 0;
+    }, 350);
+  }
 }
 
 // 倒數讀秒
@@ -79,10 +81,14 @@ const showCountDown = ref(0)
 watch(currentIndex, () => {
   if (currentIndex.value === 8) {
     stop()
-    randomIndex()
-    run()
+    if (previews.value.length) {
+      randomIndex()
+      setTimeout(() => {
+        run()
+      }, 1950);
+    }
   }
-  showCountDown.value = 8 - currentIndex.value
+  // showCountDown.value = 8 - currentIndex.value
 })
 
 // 結束倒數讀秒
@@ -96,34 +102,91 @@ onUnmounted(() => {
   intervalId.value = 0
 })
 
-// // 自動倒數計時控制 ==============================
-// const countDown = ref(0)
+// ==============
+const audioRef = ref(null);
+const isPlaying = ref(false);
+const currentTime = ref(0);
+const duration = ref(0);
+const currentAudioSrc = animalMusic
 
-// // 重置倒數讀秒
-// const refreshCountDown = () => {
-//   stop()
-//   showCountDown.value = countDown.value
-//   run()
-// }
+function togglePlay() {
+  if (audioRef.value) {
+    if (isPlaying.value) {
+      audioRef.value.pause()
+    } else {
+      const p = audioRef.value.play()
+      if (p && p.then) {
+        p.then(() => { isPlaying.value = true }).catch((err) => { console.warn('play failed', err) })
+      } else {
+        isPlaying.value = true
+      }
+    }
+  }
+}
+
+function updateTime() {
+  currentTime.value = audioRef.value.currentTime;
+}
+
+function loadedMetadata() {
+  duration.value = audioRef.value.duration;
+}
+
+onMounted(() => {
+  // 初始狀態設定 (可選)
+});
+
+// ==============
+const gameStatus = ref(false)
+const gameStart = () => {
+  // 檢查是否有上傳圖片
+  if (previews.value.length) {
+    gameStatus.value = !gameStatus.value
+    togglePlay() // 開始播放音樂
+    setTimeout(() => {
+      stop()
+      run()
+    }, 4000);
+  } else {
+    alert('請先上傳圖片！')
+  }
+}
 
 </script>
 
 <template>
   <div class="container-fluid">
     <div class="row">
+
+      <div class="col-12" v-if="imageIndex.length">
+        <button class="btn btn-lg btn-success" @click="gameStart()">{{ isPlaying ? '結束遊戲' : '開始遊戲' }}</button>
+
+        <audio ref="audioRef" :src="currentAudioSrc" @timeupdate="updateTime" @loadedmetadata="loadedMetadata"
+          @play="isPlaying = true" @pause="isPlaying = false" @ended="isPlaying = false" preload="metadata"></audio>
+
+        <!-- <span> -->
+        <!-- <button class="btn btn-info" @click="togglePlay">{{ isPlaying ? 'II' : '>' }}</button> -->
+
+        <!-- 進度條 -->
+        <span class="process" v-if="currentAudioSrc">{{ Math.floor(Math.floor(currentTime) / Math.floor(duration) * 100)
+        }}%</span>
+        <!-- </span> -->
+      </div>
+
       <div class="col-12">
-        <h1 class="h1" v-if="showCountDown">{{ showCountDown }}</h1>
-        <h1 class="h1" v-else>準備開始遊戲囉！</h1>
-        <input ref="uploadFileRef" type="file" name="imgUpload" multiple="multiple"
+        <!-- <h1 class="h1" v-if="showCountDown">倒數 {{ showCountDown }} 秒！</h1>
+        <h1 class="h1" v-else>準備開始遊戲囉！</h1> -->
+
+        <!-- 上傳圖片 -->
+        <input v-if="!previews.length" ref="uploadFileRef" type="file" name="imgUpload" multiple="multiple"
           @change="changeFile($event)" />
       </div>
     </div>
 
-    <div class="row" v-if="imageIndex.length > 0">
+    <div class="row show-area" v-if="imageIndex.length > 0 && gameStatus">
       <template v-for="(i, index) in imageIndex" :key="index">
         <div class="col-3" :class="`${currentIndex === index ? 'highlight' : 'show'}`">
           <img class="image-item" :id="`image-${index}`" :src="previews[i]?.url" :alt="previews[i]?.name" />
-          <p>{{ index }}</p>
         </div>
       </template>
     </div>
@@ -138,6 +201,10 @@ onUnmounted(() => {
   left: 0;
   z-index: -100;
   opacity: 0;
+}
+
+.show-area {
+  margin-top: 20px;
 }
 
 .show {
@@ -162,5 +229,11 @@ onUnmounted(() => {
   height: 300px;
   overflow: hidden;
   margin: 0px 1px 1px 1px;
+}
+
+.process {
+  margin-left: 20px;
+  font-size: 1.2rem;
+  font-weight: bold;
 }
 </style>
